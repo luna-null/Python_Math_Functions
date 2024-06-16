14f17bdd946fe523aa1a688bac40d117f153c4c2
Autumn Luna Mable <127723191+luna-null@users.noreply.github.com>
2024-06-16 16:38:31 -0700

    Update random_walks.py

--- random_walks.py
+++ random_walks.py
@@ -6,14 +6,26 @@ def random_walk(n):
 
     x0 = 0
     y0 = 0
-
+    plt.plot(x0,y0,marker='o',ms=15,c='orange')
     for i in range(0,n):
-         x1 = x0 + rand.randint(-5,5)
-         y1 = y0 + rand.randint(-5,5)
-         X = np.linspace(x0,x1,50)
-         Y = np.linspace(y0,y1,50)
-         plt.plot(X,Y,color="blue")
-         x0 = x1
-         y0 = y1
+        r = rand.randrange(1,5)
+        match r:
+            case 1:
+                x1 = x0 + 1
+                y1 = y0
+            case 2:
+                x1 = x0
+                y1 = y0 + 1
+            case 3:
+                x1 = x0 - 1
+                y1 = y0
+            case 4:
+                x1 = x0
+                y1 = y0 - 1
+        X = np.linspace(x0,x1,20)
+        Y = np.linspace(y0,y1,20)
+        plt.plot(X,Y,marker = ',')
+        x0 = x1
+        y0 = y1
+    plt.plot(x1,y1,marker = 'o',ms=15,c='black')
     plt.show()
-
