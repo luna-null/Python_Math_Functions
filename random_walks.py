51fe6f5d78406da70763acb554a34f50af884bbd
Autumn Luna Mable <127723191+luna-null@users.noreply.github.com>
2024-06-16 16:39:44 -0700

    Update random_walks.py

--- random_walks.py
+++ random_walks.py
@@ -6,7 +6,7 @@ def random_walk(n):
 
     x0 = 0
     y0 = 0
-    plt.plot(x0,y0,marker='o',ms=15,c='orange')
+    plt.plot(x0,y0,marker='o',ms=10,c='black')
     for i in range(0,n):
         r = rand.randrange(1,5)
         match r:
@@ -27,5 +27,5 @@ def random_walk(n):
         plt.plot(X,Y,marker = ',')
         x0 = x1
         y0 = y1
-    plt.plot(x1,y1,marker = 'o',ms=15,c='black')
+    plt.plot(x1,y1,marker = 'o',ms=10,c='orange')
     plt.show()
